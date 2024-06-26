bd775cb53388c46110e8174b906a23d1e8294a1f
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-06-26 19:52:55 +0100

    Modified 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  Promise.allSettled([photoPromise, userPromise]).then((res) => {
-    console.log(res);
+  return Promise.allSettled([signUpUser(firstName, lastName),
+    uploadPhoto(fileName)]).then((res) => {
+    for (const obj of res) {
+      if ('reason' in obj) {
+        obj.value = obj.reason;
+        delete obj.reason;
+      }
+    }
+    return res;
   });
 }
