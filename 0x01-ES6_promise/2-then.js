43cacbc4e27822b767c340325baee6d7bfd278db
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-06-26 21:52:15 +0100

    Modified 6-final-user.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'Success',
-      };
-    })
-    .catch(() => {
-      const err = new Error();
-      console.log('Got a response from the API');
-      return err;
-    });
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
