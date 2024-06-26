69d49403534c4b3dcacdbd3b69b1a53d6d49d37e
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-06-26 18:47:16 +0100

    Modified 2-then.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const imgPromise = uploadPhoto();
-  const userPromise = createUser();
-
-  Promise.all([imgPromise, userPromise]).then((data) => {
+  Promise.all([uploadPhoto(), createUser()]).then((data) => {
     console.log(data[0].body, data[1].firstName, data[1].lastName);
   }).catch(() => {
     console.log('Signup system offline');
