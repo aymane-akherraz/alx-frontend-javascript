1f0c261f7bf6f4f4b1bc31b9f84fc06d9e9bd6da
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-07-01 21:48:34 +0100

    Modified 8-

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,5 +2,5 @@ export default function cleanSet(set, startString) {
   if (startString.length === 0) {
     return '';
   }
-  return [...set].filter((e) => typeof e === 'string' && e.startsWith(startString)).map((e) => e.slice(startString.length)).join('-');
+  return Array.from(set).filter((e) => typeof e === 'string' && e.startsWith(startString)).map((e) => e.slice(startString.length)).join('-');
 }
